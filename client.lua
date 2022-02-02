bdeb182643eb000eeb4480a340f440893762abdd
Tafsir Bhuiyan <95275471+Tafsir-Bhuiyan@users.noreply.github.com>
2022-02-03 00:14:46 +0600

    Update

--- client.lua
+++ client.lua
@@ -1,14 +1,5 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(10)
-        if QBCore == nil then
-            TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
-            Citizen.Wait(200)
-        end
-    end
-end)
 
 		
 local PlayerData            = {}	
